fa0dc2d88a7d2b0689f47bead354ba7c06328dd8
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-09-30 15:17:00 +0100

    singles working, double needs sorting (trying with hardcoded 5)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const counter = document.getElementById('counter');
 
 let count = 501;
 counter.innerText = String(count);
-let singleArr = [];
+let idArray = [];
 
 bullseye.addEventListener('click', () => {
     count = 'bullseye!';
@@ -13,16 +13,25 @@ bullseye.addEventListener('click', () => {
 
 for (let i = 1; i < 21; i++) {
     let currentId = i;
-    singleArr.push(currentId);
+    idArray.push(currentId);
 }
+//mapping for single scores
+idArray.map((id) => {
+    //mapping for single scores
+    let section1 = document.getElementById(`sngl_${id}`);
 
-console.log(singleArr);
-
-singleArr.map((id) => {
-    let section = document.getElementById(`sngl_${id}`);
-    section.addEventListener('click', () => {
+    section1.addEventListener('click', () => {
         count -= id;
         counter.innerText = String(count);
     });
-    console.log(section);
+    console.log(idArray);
 });
+
+//double
+let section2 = document.getElementById('dbl_5');
+section2.addEventListener('click', () => {
+    count -= 5 * 2;
+    counter.innerText = String(count);
+    console.log(section2);
+});
+// debugger;
