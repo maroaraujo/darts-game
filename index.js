3c82dc417a8c0d8e2e6f47a6dc03c43e3138483f
Maria <maroaraujo@gmail.com>
2022-09-30 17:58:09 +0200

    added the turns and scores of doubles and triples

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const bullseye = document.getElementById('bullseye');
+const sngl25 = document.getElementById('sngl_25')
 const counter = document.getElementById('counter');
+let turnsList = document.getElementById('turns-list');
 
+let turnsCounter = 0
 let count = 501;
 counter.innerText = String(count);
 let idArray = [];
@@ -8,8 +11,23 @@ let idArray = [];
 bullseye.addEventListener('click', () => {
     count -= 50;
     counter.innerText = String(count);
+    turnsCounter += 50
+    let scoreList = document.createElement("li")
+    scoreList.innerText = String(50)
+    turnsList.appendChild(scoreList)
     console.log(count);
 });
+
+sngl25.addEventListener('click', () => {
+    count -= 25;
+    counter.innerText = String(count);
+    turnsCounter += 25
+    let scoreList = document.createElement("li")
+    scoreList.innerText = String(25)
+    turnsList.appendChild(scoreList)
+
+    console.log(count);
+})
 
 for (let i = 1; i < 21; i++) {
     let currentId = i;
@@ -23,16 +41,42 @@ idArray.map((id) => {
     section1.addEventListener('click', () => {
         count -= id;
         counter.innerText = String(count);
+        turnsCounter += id
+        let scoreList = document.createElement("li")
+        scoreList.innerText = String(id)
+        turnsList.appendChild(scoreList)
     });
     console.log(idArray);
     let section2 = document.getElementById(`dbl_${id}`);
     section2.addEventListener('click', () => {
-        count -= id * 2;
+         count -= id * 2;       
         counter.innerText = String(count);
+        turnsCounter += id *2
+        let scoreList = document.createElement("li")
+        scoreList.innerText = String(id*2)
+        turnsList.appendChild(scoreList)
         console.log(section2);
     });
+    let section3 = document.getElementById(`trpl_${id}`);
+    section3.addEventListener('click', () => {
+        count -= id * 3;
+        counter.innerText = String(count);
+        turnsCounter += id *3
+        let scoreList = document.createElement("li")
+        scoreList.innerText = String(id*3)
+        turnsList.appendChild(scoreList)
+        console.log(section3);
+    });
 });
+
+if(count === 0){
+    counter.innerText = "Checked out! :D";
+} 
 
 //double
 
 // debugger;
+
+//RULES
+// You have to finish on a double OR bullseye
+//
