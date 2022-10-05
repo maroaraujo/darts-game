fbfe5780ca7f68105a6ee64f472cd4c1f51a1f58
Maria <maroaraujo@gmail.com>
2022-10-05 12:52:50 +0200

    creating logic for the end game, added e.target for handleClick

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ const sngl25 = document.getElementById('sngl_25')
 const counter = document.getElementById('counter');
 let turnsList = document.getElementById('turns-list');
 
-let count = 20;
+let count = 501;
 counter.innerText = String(count);
 let turnsArray = [];
 let idArray = [];
+function clearList(){
+    
+}
 
-function handleClick(num){
+function handleClick(num, e){
     count -= num;
     counter.innerText = String(count);
     let scoreList = document.createElement("li");
     scoreList.innerText = String(num);
+    
 if(turnsArray.length === 3){
     turnsArray = [num] ;
     //We used the same line 3 times because once the first child is removed the next child becomes the first child. 
@@ -31,17 +35,18 @@ if(turnsArray.length === 3){
     console.log(turnsArray);
 }
 
-if(count === 0){
+if(count === 0 && e.target.id.includes("dbl")){
     counter.innerText = "Checked out! :D";
-} else if (count < 0) {
+} else if (count < 0 || count === 1) {
     count = count + turnsArray.reduce((prev, cur) => prev + cur, 0);
     counter.innerText = String(count);
+
 }
 
 console.log("count just changed", count)}
 
-bullseye.addEventListener('click', () => handleClick(50));
-sngl25.addEventListener('click', () => handleClick(25)
+bullseye.addEventListener('click', (e) => handleClick(50,e));
+sngl25.addEventListener('click', (e) => handleClick(25,e)
 );
 
 for (let i = 1; i < 21; i++) {
@@ -57,9 +62,9 @@ idArray.map((id) => {
     let doublePoints = document.getElementById(`dbl_${id}`);
     let triplePoints = document.getElementById(`trpl_${id}`);
 
-    singlePoints.addEventListener('click', () => handleClick(id));
-    doublePoints.addEventListener('click', () => handleClick((id * 2)));
-    triplePoints.addEventListener('click', () => handleClick((id * 3)));
+    singlePoints.addEventListener('click', (e) => handleClick(id,e));
+    doublePoints.addEventListener('click', (e) => handleClick((id * 2),e));
+    triplePoints.addEventListener('click', (e) => handleClick((id * 3),e));
 
 
 
